78c4f55dd9d6a5491043db4156ae552adc269643
Felix Warschewski <fwarschewski@aixitem.de>
2020-06-21 12:46:31 +0200

    nodeklasse

---
+++ GroProVorb/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroProVorb
+{
+    class Node
+    {
+        Node left;
+        Node right;
+        int value;
+
+        public Node() { }
+
+        public Node(Node left, Node right, int val)
+        {
+            this.left = left;
+            this.right = right;
+            this.value = val;
+        }
+    }
+}
